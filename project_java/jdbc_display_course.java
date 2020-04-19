8f47505d6a31fd7c4882ad4040bda4a0122388e7
Gavin Glenn <43220039+veths@users.noreply.github.com>
2020-04-19 15:52:55 -0500

    6

--- project_java/jdbc_display_course.java
+++ project_java/jdbc_display_course.java
@@ -22,7 +22,7 @@ public class jdbc_display_course {
         dep = args[0];
 
         // For debugging purposes: Show the database after the insert
-        String query1 = 
+        String query1 = "";
         builder.append("<br><br><br> Courses from Department" + dep + "after:" + myDB.query(query1));
         System.out.println(builder.toString());
 
